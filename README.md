e1b042ba0b7c0b1f6c501c9fe37ef7722babc11a
Noeeekr <cardozoandre0101@gmail.com>
2024-03-05 18:28:26 -0400

    design and responsivity update

--- README.md
+++ README.md
@@ -1,17 +1,15 @@
 LOGIN PAGE
 
-1 make the "Acessar" button in loginPage send you to mainPage.html
-
-2 make a password and email check in loginPage.js
-
-3 make the password and email check enable the "Acessar" button in loginPage.html
-
-4 make a "Acessar" button decoration for each state (disabled and normal)
-
-5 view password button in input
-
 6 simulate the cookies button
 
 7 simulate the message that appears when the login inputs value are wrong
+
+8 do the media changes for different devices 
+
+9 do the footer 
+
+10 do the feedback menu
+
+11 add the logo
 
 MAIN PAGE
--- src/javascript/loginPage.js
+++ src/javascript/loginPage.js
@@ -1,8 +1,8 @@
 (function(){
     [...document.querySelectorAll(".chooseTab li")].forEach((btn) => {
         btn.addEventListener("click",(e) => {
-            changeBtnState(e,"UL");
             changeTab(e);
+            changeBtnState(e,"UL");
         })
     });
     [...document.querySelectorAll(".signInInput")].forEach((input) => {
@@ -12,10 +12,29 @@
         input.addEventListener("blur",(e) => {
             changeBtnState(e,"DIV",[".inputDecoration"],true)
         })
+        input.addEventListener("keyup",(e) => {
+            enableSendForm(e,document.querySelector(".signInTab button.submit"))
+        })
     });
+    document.querySelector("form.signInTab").addEventListener("submit",(e) => {
+        changePage(e);
+    })
 })();
 
+function enableSendForm(event,button) {
+    if (!validateForm(event)) {
+        button.setAttribute("disabled","");
+        return;
+    }
+    button.removeAttribute("disabled");
+}
+function changePage(event) {
+    event.preventDefault();
 
+    if (!validateForm(event)) return;
+
+    window.location.href = "mainPage.html";
+}
 function changeBtnState(eventOrElemen,parent,extraActives,unactivateExtras) { // html or boolean ; parent html ; array ;
     let element = eventOrElemen.target ? eventOrElemen.target : eventOrElemen;
 
@@ -39,7 +58,9 @@ function changeBtnState(eventOrElemen,parent,extraActives,unactivateExtras) { //
         })
     }
 }
-function changeTab() {
+function changeTab(event) {
+    if ([...event.target.classList].includes("active")) return;
+
     let next = document.querySelector(".tab.unactive");
     let current = document.querySelector(".tab.active");
 
@@ -48,4 +69,22 @@ function changeTab() {
     
     current.classList.add("unactive");
     current.classList.remove("active");
+}
+
+function validateForm(event) {
+    let form = event.target;
+    while (form.tagName !== "FORM") {
+        form = form.parentElement;
+    }
+    
+    let [emailInput,passwordInput] = form.querySelectorAll("input");
+    if (!(
+        emailInput.value.match(/[\w\d]+?@[\w]+?\.(com|org)/g)
+        && emailInput.value.match(/[\w\d]+?@[\w]+?\.(com|org)/g)[0] === emailInput.value
+        && passwordInput.value.length > 5
+    )) {
+        return false;
+    }
+
+    return true;
 }
