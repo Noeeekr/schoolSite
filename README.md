31fbe26a5f24ac1e1a3e74ed65c5c0fa8c510181
Noeeekr <cardozoandre0101@gmail.com>
2024-03-13 20:23:56 -0400

    started working with webpack & babel

--- README.md
+++ README.md
@@ -3,9 +3,14 @@ Try to reproduce school site
 
 
 
-This project might evolve with my abilities
+This project might evolve with my abilities, including:
 
-
+> HTML
+> CSS
+> JAVASCRIPT
+> NPM
+> with babel
+> with webpack
 
 
 I'll see if I can create versions for each technology that I've used on it 
---
+++ frontend/webpack.config.js
@@ -0,0 +1,58 @@
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
+
+const path = require('path');
+
+const isProduction = process.env.NODE_ENV == 'production';
+
+const stylesHandler = 'style-loader';
+
+const config = {
+    mode: "development",
+    target: ["web","es5"],
+    entry: './src/components/loginPage.js',
+    output: {
+        filename: "loginPage.js",
+        path: path.resolve(__dirname, 'dist','components')
+    },
+    plugins: [
+        // Add your plugins here
+        // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+    ],
+    module: {
+        rules: [
+            {   
+                exclude: /node_modules/,
+                test: /\.(js|jsx)$/i,
+                loader: 'babel-loader',
+                options: {
+                    presets: [
+                        "@babel/preset-env"
+                    ],
+                    plugins: [
+                        ["@babel/plugin-transform-runtime"]
+                    ]
+                }
+            },
+            {
+                test: /\.css$/i,
+                use: [stylesHandler,'css-loader'],
+            },
+            {
+                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+                type: 'asset',
+            },
+
+            // Add your rules for custom modules here
+            // Learn more about loaders from https://webpack.js.org/loaders/
+        ],
+    },
+};
+
+module.exports = () => {
+    if (isProduction) {
+        config.mode = 'production';
+    } else {
+        config.mode = 'development';
+    }
+    return config;
+};
