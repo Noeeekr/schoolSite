0437a03e40dfc92a0cdec5d7adc7d2bf3dff0cfe
Noeeekr <cardozoandre0101@gmail.com>
2024-03-19 13:04:46 -0400

    navigation design update

--- frontend/src/components/mainPage.js
+++ frontend/src/components/mainPage.js
@@ -1,1 +1,34 @@
 // do the student icon display the first letter of student's name
+
+(function() {{
+    document.querySelector(".generalNav_button").addEventListener("click",(e) => {
+        changeBtnState(e,"unactive");
+        changeTabState(e);
+    })
+}})();
+
+function switchActiveBtn() {
+
+}
+function changeBtnState(event,state) {
+    if (event.target.getAttribute(state)) {
+        event.target.removeAttribute(state)
+        return;
+    }
+    event.target.setAttribute(state,"true");
+}
+function changeTabState(event) {
+
+}
+// change state should change tab state 
+
+
+console.log(
+    document.querySelectorAll(".generalNavigation .nav .item:has(.menu.expand)")
+)
+
+document.querySelectorAll(".generalNavigation .nav .item:has(.menu.expand)").forEach(
+    m => {
+        m.querySelector(".innerHolder").style = "display: none"
+    }
+)
